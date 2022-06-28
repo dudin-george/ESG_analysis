0cdfcdf72cfd2c16eb892f2b1ef5910dbad71cab
Samoed <samoed.roman@gmail.com>
2022-06-28 19:25:09 +0500

    working compose

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from parser.SravniReviews import SravniReviews
-
+import schedule
 from model.Database import Database
+import threading
+from typing import Callable
+from misc.Logger import get_logger
+
+
+def run_threaded(job_func: Callable) -> None:
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
 
 
 def main() -> None:
     Database()
+    logger = get_logger(__name__)
+    logger.info("start app")
+    get_logger("schedule")
 
     sravni_parser = SravniReviews()
-    sravni_parser.parse()
+    schedule.every(10).minute.do(run_threaded, sravni_parser.parse)
+
+    while True:
+        schedule.run_pending()
 
 
 if __name__ == "__main__":
