439f623b172072042c223d3e950561bc9d9b4f87
Samoed <samoed.roman@gmail.com>
2022-10-02 17:05:06 +0500

    add env variable for real_page

--- api/tests/test_cbr_parser.py
+++ api/tests/test_cbr_parser.py
@@ -1,7 +1,13 @@
+import pytest
+
 from app.bank_parser import CBRParser
 from app.query.bank import get_bank_count
+import os
+
+TEST_REAL_PAGE = os.getenv("TEST_REAL_PAGE", "False").lower() == "true"
 
 
+@pytest.mark.skipif(not TEST_REAL_PAGE, reason="ddos cbr")
 def test_get_real_page(session):
     CBRParser(session).load_banks()
     with session as db:
