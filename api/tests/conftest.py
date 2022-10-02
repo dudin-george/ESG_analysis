eaeb101647d177d06069bbaf0a93561f9fb5a294
Samoed <samoed.roman@gmail.com>
2022-10-02 14:12:09 +0500

    add mock for bank_list request

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -8,10 +8,11 @@ from sqlalchemy_utils import (  # type: ignore
     database_exists,
     drop_database,
 )
-
+from bs4 import BeautifulSoup
 from app.database import get_db
 from app.database.base import Base
 from app.main import app
+from app.bank_parser import CBRParser
 from app.settings import Settings
 
 
@@ -20,32 +21,46 @@ class DataBase:
     TestingSessionLocal: sessionmaker = None
 
 
-database = DataBase()
+test_database = DataBase()
+
+
+@pytest.fixture
+def session():
+    return test_database.TestingSessionLocal()
 
 
 @pytest.fixture(scope="function", autouse=True)
 def setup():
-    database.engine = create_engine(Settings().database_url, echo=False)
-    database.TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=database.engine)
-    if not database_exists(database.engine.url):
-        create_database(database.engine.url)
+    test_database.engine = create_engine(Settings().database_url, echo=False)
+    test_database.TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_database.engine)
+    if not database_exists(test_database.engine.url):
+        create_database(test_database.engine.url)
 
-    Base.metadata.create_all(bind=database.engine)
+    Base.metadata.create_all(bind=test_database.engine)
     yield
-    drop_database(database.engine.url)
+    drop_database(test_database.engine.url)
 
 
 def override_get_db():
-    db = database.TestingSessionLocal()
+    db = test_database.TestingSessionLocal()
     try:
         yield db
     finally:
         db.close()
 
 
+@pytest.fixture(scope="session")
+def cbr_page():
+    with open("html_pages/cbr_page.html", "r") as f:
+        cbr_page = f.read()
+    return BeautifulSoup(cbr_page, "html.parser")
+
+
 @pytest.fixture
-def client():
+def client(mocker, cbr_page):
+    mocker.patch("app.bank_parser.CBRParser.get_page", return_value=cbr_page)
     app.dependency_overrides[get_db] = override_get_db
+    CBRParser(test_database.TestingSessionLocal()).load_banks()
     return TestClient(app)
 
 
