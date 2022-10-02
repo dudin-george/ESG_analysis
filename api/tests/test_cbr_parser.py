994477f8bf9bc83c9353e2d6ffa449e3b9506467
Samoed <samoed.roman@gmail.com>
2022-10-02 14:59:09 +0500

    add cbr parser

---
+++ api/tests/test_cbr_parser.py
@@ -0,0 +1,22 @@
+from app.bank_parser import CBRParser
+from app.query.bank import get_bank_count
+
+
+def test_get_real_page(session):
+    CBRParser(session).load_banks()
+    with session as db:
+        assert get_bank_count(db) > 300
+
+
+def test_get_page(session, cbr_page):
+    cbr = CBRParser(session)
+    cbr.get_page = lambda: cbr_page
+    with session as db:
+        cbr.load_banks()
+        assert get_bank_count(db) > 300
+
+
+def test_bank_list(session, cbr_page):
+    cbr = CBRParser(session)
+    banks = cbr.get_bank_list(cbr_page)
+    assert len(banks) > 300
