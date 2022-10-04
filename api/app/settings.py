a8b8ae50050e09b7699a6df5a5f4d22ce3dee384
Samoed <samoed.roman@gmail.com>
2022-10-04 15:18:12 +0500

    add echo in env

--- api/app/settings.py
+++ api/app/settings.py
@@ -3,4 +3,5 @@ from pydantic import BaseSettings, Field, PostgresDsn
 
 class Settings(BaseSettings):
     database_url: PostgresDsn = Field(env="DATABASE_URL")
+    echo: bool = Field(env="ECHO", default=False)
     # broker_url: AmqpDsn = Field(env="BROKER_URL")
