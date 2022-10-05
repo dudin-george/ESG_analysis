ef4507cc43d2c4e709a5d5f1eae5040584542f02
Samoed <samoed.roman@gmail.com>
2022-10-05 12:04:15 +0500

    make one main file with argparse

--- parser/main.py
+++ parser/main.py
@@ -1,20 +1,21 @@
 from logging import getLogger
 from time import sleep
+from typing import Type
 
 import schedule  # type: ignore
 from sqlalchemy_utils import create_database, database_exists  # type: ignore
 
+from utils.arg_parser import parse_args
+from utils.base_parser import BaseParser
 from utils.database import Base, engine
 from utils.logger import get_logger
 from utils.settings import Settings
-from sravni_reviews.sravni_reviews import SravniReviews
 
 
-def parsers_setup() -> None:
-    sravni_reviews = SravniReviews()
-    sravni_reviews.parse()
+def parsers_setup(parser: type[BaseParser]) -> None:
+    parser().parse()
     getLogger("schedule")
-    schedule.every().day.do(sravni_reviews.parse)
+    schedule.every().day.do(parser.parse)
 
     while True:
         schedule.run_pending()
@@ -24,11 +25,12 @@ def main() -> None:
     sleep(5)
     logger = get_logger(__name__, Settings().logger_level)
     logger.info("start app")
+    parser = parse_args()
     if not database_exists(engine.url):
         create_database(engine.url)
     Base.metadata.create_all(bind=engine)
     logger.info("create db")
-    parsers_setup()
+    parsers_setup(parser)
 
 
 if __name__ == "__main__":
