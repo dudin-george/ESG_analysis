b94ee217bd23e0b858c19ced90ac1391f8fba88f
Samoed <samoed.roman@gmail.com>
2022-10-13 00:23:31 +0500

    add env file

--- api/app/settings.py
+++ api/app/settings.py
@@ -3,7 +3,12 @@ from pydantic import BaseSettings, Field, PostgresDsn
 
 class Settings(BaseSettings):
     database_url: PostgresDsn = Field(
-        env="DATABASE_URL", default=PostgresDsn("postgresql+psycopg2://myusername:mypassword@localhost/database")
+        env="DATABASE_URL"
     )
     echo: bool = Field(env="ECHO", default=False)
     # broker_url: AmqpDsn = Field(env="BROKER_URL")
+
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
