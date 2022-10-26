6a9a68b26f8f5d5ef22415960accb19939cf8845
Samoed <samoed.roman@gmail.com>
2022-10-26 16:08:53 +0500

    remove timezone

--- api/app/query/text.py
+++ api/app/query/text.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
-from sqlalchemy import select
+from sqlalchemy import func, select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database import Bank, Source, TempSentence, Text, TextResult, TextSentence
+from app.database import Bank, Source, Text, TextResult, TextSentence
 from app.exceptions import IdNotFoundError
 from app.schemes.text import GetTextSentencesItem, PostTextItem
 from app.tasks.transform_texts import transform_texts
@@ -27,9 +27,11 @@ async def create_text_sentences(db: AsyncSession, post_texts: PostTextItem) -> N
         bank = await db.scalar(select(Bank).filter(Bank.id == text_db_item.bank_id))
         if source is None or bank is None:
             raise IdNotFoundError("Source or bank not found")
-        if (post_texts.parser_state or post_texts.date) and len(text_db) > 0:
-            source.parser_state = post_texts.parser_state
-            source.last_update = post_texts.date
+        if len(text_db) > 0:
+            if post_texts.parser_state:
+                source.parser_state = post_texts.parser_state
+            if post_texts.date:
+                source.last_update = post_texts.date.replace(tzinfo=None)
             db.add(source)
     db.add_all(text_db)
     try:
@@ -53,39 +55,40 @@ def _generate_table_name(model_id: int, sources: list[str]) -> str:
     return f"table_model_{model_id}_" + "_".join(sources)
 
 
-async def insert_data(db: AsyncSession, model_id: int, sources: list[str], table_name: str) -> None:
+async def insert_new_sentences(db: AsyncSession, model_id: int, sources: list[str]) -> None:
     text_result_subq = select(TextResult).filter(TextResult.model_id == model_id).subquery()
     query = (
-        select(TextSentence.id, TextSentence.sentence)
+        select(TextSentence.id)
         .join(TextSentence.text)
         .join(Text.source)
         .join(text_result_subq, TextSentence.id == text_result_subq.c.text_sentence_id, isouter=True)
         .filter(Source.site.in_(sources))
         .filter(text_result_subq.c.text_sentence_id == None)  # noqa: E711
-        .limit(100_000)
     )
-    items = []
-    for sentence_item in await db.scalar(query):
-        items.append(
-            TempSentence(sentence_id=sentence_item["id"], sentence=sentence_item["sentence"], query=table_name)
-        )
-    db.add_all(items)
+    sentence_ids = await db.execute(query)
+    for sentence_id in sentence_ids:
+        db.add(TextResult(text_sentence_id=sentence_id.id, model_id=model_id, is_processed=False))
     await db.commit()
 
 
 async def get_text_sentences(
     db: AsyncSession, model_id: int, sources: list[str], limit: int
 ) -> list[GetTextSentencesItem]:
-    text_result_subq = select(TextResult).filter(TextResult.model_id == model_id).subquery()
-    query = (
-        select(TextSentence.id, TextSentence.sentence)
-        .join(TextSentence.text)
-        .join(Text.source)
-        .join(text_result_subq, TextSentence.id == text_result_subq.c.text_sentence_id, isouter=True)
-        .filter(Source.site.in_(sources))
-        .filter(text_result_subq.c.text_sentence_id == None)  # noqa: E711
-        .limit(limit)
+    unused_model_sentences = await db.scalar(
+        select(func.count(TextResult.id))
+        .filter(TextResult.model_id == model_id)
+        .filter(TextResult.is_processed == False)  # noqa: E712
     )
+    if unused_model_sentences == 0:
+        await insert_new_sentences(db, model_id, sources)
+    select_unused_sentence_ids = (
+        select(TextResult.text_sentence_id)
+        .filter(TextResult.model_id == model_id)
+        .filter(TextResult.is_processed == False)  # noqa: E712
+        .limit(limit)
+        .subquery()
+    )
+    query = select(TextSentence.id, TextSentence.sentence).filter(TextSentence.id.in_(select_unused_sentence_ids))
     items = []
     for sentence in await db.execute(query):
         items.append(GetTextSentencesItem(sentence_id=sentence.id, sentence=sentence.sentence))
