08dc816fabedd9df4a464270e0bbcb309eb4c22f
Samoed <samoed.roman@gmail.com>
2023-01-12 01:48:19 +0500

    vk tests

--- parser/tests/mixins.py
+++ parser/tests/mixins.py
@@ -3,5 +3,6 @@ import pytest
 
 class TestMixin:
     @pytest.fixture(autouse=True, scope="function")
-    def setup(self, session):
+    def setup(self, session, mocker):
         self.session = session
+        mocker.patch("common.base_parser.sleep", return_value=None)
