4ab0837286ee9b47e67ab8c7d6bc0b13207ea030
Roman Solomatin <36135455+Samoed@users.noreply.github.com>
2023-05-17 22:04:18 +0500

    remove soup

--- api/app/dataloader/base_parser.py
+++ api/app/dataloader/base_parser.py
@@ -1,17 +1,21 @@
+from typing import Any
+
 import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 from fastapi.logger import logger
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.models.bank import Bank, BankType
 from app.query import bank as query
+from app.utils import get_dataframe
 
 
 class BaseParser:
     logger = logger
     bank_type: BankType
     URL = ""
+    SKIP_ROWS = 0
+    INDEX_COL = None
+    TYPES: dict[str, Any] = {}
 
     def __init__(self, db: AsyncSession) -> None:
         self.db = db
@@ -26,37 +30,14 @@ class BaseParser:
     async def create_bank_type(self) -> BankType:
         raise NotImplementedError
 
-    def send_get(self, url: str, headers: dict[str, str] | None = None) -> requests.Response | None:
-        if headers is None:
-            headers = {}
-        response = requests.get(url, headers=headers)
-        if response.status_code == 403:
-            self.logger.error("cbr.ru 403 error")
-            return None
-        return response
-
-    def get_page(self, url: str, headers: dict[str, str] | None = None) -> BeautifulSoup | None:
-        response = self.send_get(url, headers)
-        if response is None:
-            return None
-        page = BeautifulSoup(response.text, "html.parser")
-        return page
-
-    def get_dataframe(self, url: str, skip_rows: int = 3, index_col: str | int | None = None) -> pd.DataFrame | None:
-        response = self.send_get(url)
-        if response is None:
-            return None
-        dataframe = pd.read_excel(response.content, skiprows=skip_rows, index_col=index_col)
-        return dataframe
-
-    def get_bank_list(self, page: BeautifulSoup) -> list[Bank]:
+    def get_bank_list(self, df: pd.DataFrame) -> list[Bank]:
         raise NotImplementedError
 
     async def parse(self) -> None:
         self.logger.info("start download bank list")
-        page = self.get_page(self.URL)
-        if page is None:
+        df = get_dataframe(self.URL, skip_rows=self.SKIP_ROWS, index_col=self.INDEX_COL, types=self.TYPES)
+        if df is None:
             self.logger.error("cbr.ru 403 error")
             raise Exception("cbr.ru 403 error")
-        banks = self.get_bank_list(page)
+        banks = self.get_bank_list(df)
         await query.load_banks(self.db, banks)
